b145a1376c5d3b2f379ddfed1e92911456913ac3
Pedro André <pedro@sentrytechnologies.ai>
2025-10-09 13:53:54 +0200

    drivers: sensor: sch16t: read and decode driver
    This adds the initial driver for the Murata SCH16T series of combined
    accelerometer and gyroscope sensors. The driver is based on the read and
    decode API since the fetch and get API is planned to be deprecated.
    
    The SCH16T can be interfaced with using a classic register read/write
    protocol. The SPI frames are constructed according to the SafeSPI 2.0
    specification. As a result, the device supports both 48-bit and 32-bit SPI
    frames. In order to reduce complexity, only 32-bit frames are supported in
    this driver. No features should be lost by opting to use 32-bit instead of
    48-bit.
    
    The SCH16T supports multi-slave operation on a single bus by use of the
    DRY_SYNC pin. Since this pin is also used for signaling interrupts for data
    ready, it is configured as such and multi-slave support on a single bus is
    not supported by this driver.
    
    Despite this, support for specifying values for decimation of the readings
    and respective dynamic range was implemented using device tree bindings.
    
    Signed-off-by: Pedro André <pedro@sentrytechnologies.ai>

---
+++ drivers/sensor/murata/sch16t/sch16t.h
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2025 Sentry Technologies ApS
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef ZEPHYR_DRIVERS_SENSOR_SCH16T_H_
+#define ZEPHYR_DRIVERS_SENSOR_SCH16T_H_
+
+#define DT_DRV_COMPAT murata_sch16t
+
+#include <zephyr/types.h>
+#include <zephyr/rtio/rtio.h>
+#include <zephyr/drivers/gpio.h>
+
+#define SCH16T_QUEUE_SIZE 32
+
+struct sch16t_encoded_data {
+	struct {
+		float acc_lsb;
+		float rate_lsb;
+		uint64_t timestamp;
+	} header;
+	union {
+		uint8_t buf[12];
+		struct {
+			struct {
+				int16_t x;
+				int16_t y;
+				int16_t z;
+			} acc;
+			struct {
+				int16_t x;
+				int16_t y;
+				int16_t z;
+			} rate;
+		} __packed;
+	};
+};
+
+struct sch16t_rtio_payloads {
+	int len;
+	uint32_t in[SCH16T_QUEUE_SIZE];
+	uint32_t out[SCH16T_QUEUE_SIZE];
+};
+
+struct sch16t_data {
+	struct {
+		struct rtio *ctx;
+		struct rtio_iodev *iodev;
+		struct rtio_iodev_sqe *iodev_sqe;
+		struct sch16t_rtio_payloads payloads;
+	} rtio;
+};
+
+struct sch16t_config {
+	uint8_t acc_dr;
+	uint8_t rate_dr;
+	uint8_t decimation;
+	struct gpio_dt_spec int_gpio;
+	struct gpio_dt_spec reset_gpio;
+};
+
+#endif /* ZEPHYR_DRIVERS_SENSOR_SCH16T_H_ */
---
+++ drivers/sensor/murata/sch16t/sch16t_submit.c
@@ -0,0 +1,95 @@
+/*
+ * Copyright (c) 2025 Sentry Technologies ApS
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/logging/log.h>
+#include <zephyr/drivers/sensor.h>
+
+#include "sch16t.h"
+#include "sch16t_bus.h"
+#include "sch16t_reg.h"
+#include "sch16t_submit.h"
+#include "sch16t_decoder.h"
+
+LOG_MODULE_REGISTER(SCH16T_SUBMIT, CONFIG_SENSOR_LOG_LEVEL);
+
+static const uint8_t sch16t_measu_regs[] = {
+	SCH16T_REG_ACC_X2,  SCH16T_REG_ACC_Y2,  SCH16T_REG_ACC_Z2,
+	SCH16T_REG_RATE_X2, SCH16T_REG_RATE_Y2, SCH16T_REG_RATE_Z2,
+};
+
+static void sch16t_complete_cb(struct rtio *ctx, const struct rtio_sqe *sqe, int res, void *arg)
+{
+	const struct device *dev = arg;
+	struct sch16t_data *data = dev->data;
+	struct rtio_iodev_sqe *iodev_sqe = data->rtio.iodev_sqe;
+	struct rtio_cqe *cqe;
+	int ret = 0;
+
+	do {
+		cqe = rtio_cqe_consume(ctx);
+		if (cqe != NULL) {
+			ret = cqe->result;
+			rtio_cqe_release(ctx, cqe);
+		}
+	} while (cqe != NULL);
+
+	if (ret) {
+		LOG_ERR("single fetch transaction failed (ret=%d)", ret);
+		rtio_iodev_sqe_err(iodev_sqe, ret);
+	} else {
+		rtio_iodev_sqe_ok(iodev_sqe, 0);
+	}
+}
+
+void sch16t_submit_single(const struct device *dev)
+{
+	uint32_t min_buf_len = sizeof(struct sch16t_encoded_data);
+	struct sch16t_data *data = dev->data;
+	struct rtio_iodev_sqe *iodev_sqe = data->rtio.iodev_sqe;
+	struct sch16t_encoded_data *edata;
+	uint32_t buf_len;
+	uint8_t *buf;
+	int ret;
+
+	ret = rtio_sqe_rx_buf(iodev_sqe, min_buf_len, min_buf_len, &buf, &buf_len);
+	if (ret) {
+		LOG_ERR("failed to get a read buffer of size %u bytes (ret=%d)", min_buf_len, ret);
+		rtio_iodev_sqe_err(iodev_sqe, ret);
+		return;
+	}
+
+	edata = (struct sch16t_encoded_data *)buf;
+
+	ret = sch16t_encode(dev, buf);
+	if (ret) {
+		rtio_iodev_sqe_err(iodev_sqe, ret);
+		return;
+	}
+
+	ret = sch16t_bus_sched_bulk_read(dev, ARRAY_SIZE(sch16t_measu_regs), sch16t_measu_regs,
+					 (uint16_t *)edata->buf, sch16t_complete_cb);
+	if (ret) {
+		rtio_iodev_sqe_err(iodev_sqe, ret);
+		return;
+	}
+
+	rtio_submit(data->rtio.ctx, 0);
+}
+
+void sch16t_submit(const struct device *dev, struct rtio_iodev_sqe *iodev_sqe)
+{
+	const struct sensor_read_config *cfg = iodev_sqe->sqe.iodev->data;
+	struct sch16t_data *data = dev->data;
+
+	data->rtio.iodev_sqe = iodev_sqe;
+
+	if (!cfg->is_streaming) {
+		sch16t_submit_single(dev);
+	} else {
+		LOG_ERR("streaming not supported (ret=%d)", -ENOTSUP);
+		rtio_iodev_sqe_err(iodev_sqe, -ENOTSUP);
+	}
+}
